38a03a55ca194f8d5fbaac69be4be3049649de58
Hieu Nguyen <hieunguyen2287@gmail.com>
2022-10-11 15:03:41 +0000

    11 Oct 2022, 10:03 pm

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 
 func main() {
 	// fmt.Println("Value:", rand.Int())
-	const price float32 = 275.00
-	const tax float32 = 27.5
-	const quantity = 2
-	fmt.Println("Total:", quantity * (price + tax))
+	const price, tax float32 = 275.00, 27.5
+	const quantity, inStock = 2, true
+	fmt.Println("Total:", quantity*(price+tax))
+	fmt.Println("In stock:", inStock)
 }
