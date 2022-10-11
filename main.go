507e53cd3b0426169c9215db45a9e064d69d9c36
Hieu Nguyen <hieunguyen2287@gmail.com>
2022-10-11 15:36:56 +0000

    11 Oct 2022, 10:36 pm

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// fmt.Println("Value:", rand.Int())
-	price, tax, quantity, inStock := 275.00, 27.5, 2.0, true
-	fmt.Println("Total:", quantity*(price+tax))
-	fmt.Println("In stock:", inStock)
-	price = 300
-	fmt.Println("Total:", quantity*(price+tax))
-	fmt.Println("Price:", price)
+	first := 100
+	second := &first
+
+	first++
+
+	fmt.Println("First:", first)
+	fmt.Println("Second:", second)
 }
