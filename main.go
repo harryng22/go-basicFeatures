4e1dceb55774d60bb9d2caa1581adf7c06f219bb
Hieu Nguyen <hieunguyen2287@gmail.com>
2022-10-13 15:41:05 +0000

    13 Oct 2022, 10:41 pm

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	names := []string{"Kayak", "Lifejacket", "Paddle"}
 
+	names = append(names, "Hat", "Gloves")
+
 	for index, value := range names {
 		fmt.Println("Index:", index, "Value:", value)
 	}
