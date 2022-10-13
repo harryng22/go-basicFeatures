2781e658faeb27df3c0bcf8ef687520ee461a79f
Hieu Nguyen <hieunguyen2287@gmail.com>
2022-10-13 16:04:21 +0000

    13 Oct 2022, 11:04 pm

--- main.go
+++ main.go
@@ -3,28 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	// names := []string{"Kayak", "Lifejacket", "Paddle"}
+	names := [3]string{"Kayak", "Lifejacket", "Paddle"}
 
-	// appendedNames := append(names, "Hat", "Gloves")
-	// names[0] = "Canoe"
-
-	// fmt.Println(names)
-	// fmt.Println(appendedNames)
-
-	names := make([]string, 3, 6)
-
-	names[0] = "Kayak"
-	names[1] = "LifeJacket"
-	names[2] = "Paddle"
-
-	moreNames := []string{"Hat", "Gloves"}
-
-	appendedNames := append(names, moreNames...)
+	someNames := names[1:3]
+	allNames := names[:]
 	names[0] = "Canoe"
 
-	fmt.Println(len(names))
-	fmt.Println(cap(names))
-	fmt.Println(names)
-	fmt.Println(appendedNames)
-	fmt.Println(moreNames)
+	fmt.Println(allNames)
+	fmt.Println(someNames)
+
+	// names := make([]string, 3, 6)
+
+	// names[0] = "Kayak"
+	// names[1] = "LifeJacket"
+	// names[2] = "Paddle"
+
+	// moreNames := []string{"Hat", "Gloves"}
+
+	// appendedNames := append(names, moreNames...)
+	// names[0] = "Canoe"
+
+	// fmt.Println(len(names))
+	// fmt.Println(cap(names))
+	// fmt.Println(names)
+	// fmt.Println(appendedNames)
+	// fmt.Println(moreNames)
 }
