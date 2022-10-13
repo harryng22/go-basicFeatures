7e27dd70f97b86c46b0a3242eb869f5855b33fd8
Hieu Nguyen <hieunguyen2287@gmail.com>
2022-10-13 15:50:29 +0000

    13 Oct 2022, 10:50 pm

--- main.go
+++ main.go
@@ -3,11 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	names := []string{"Kayak", "Lifejacket", "Paddle"}
+	// names := []string{"Kayak", "Lifejacket", "Paddle"}
 
-	appendedNames := append(names, "Hat", "Gloves")
-	names[0] = "Canoe"
+	// appendedNames := append(names, "Hat", "Gloves")
+	// names[0] = "Canoe"
 
-	fmt.Println(names)
-	fmt.Println(appendedNames)
+	// fmt.Println(names)
+	// fmt.Println(appendedNames)
+
+	names := make([]string, 3, 6)
+
+	names[0]="Kayak"
+	names[1]="LifeJacket"
+	names[2]="Paddle"
+
+	fmt.Println(len(names))
+	fmt.Println(cap(names))
 }
