977b319c096cc03139f1ab15ec358f01d706ad90
Hieu Nguyen <hieunguyen2287@gmail.com>
2022-10-13 15:53:43 +0000

    13 Oct 2022, 10:53 pm

--- main.go
+++ main.go
@@ -13,10 +13,15 @@ func main() {
 
 	names := make([]string, 3, 6)
 
-	names[0]="Kayak"
-	names[1]="LifeJacket"
-	names[2]="Paddle"
+	names[0] = "Kayak"
+	names[1] = "LifeJacket"
+	names[2] = "Paddle"
+
+	appendedNames := append(names, "Hat", "Gloves")
+	names[0]="Canoe"
 
 	fmt.Println(len(names))
 	fmt.Println(cap(names))
+	fmt.Println(names)
+	fmt.Println(appendedNames)
 }
